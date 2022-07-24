d8c19173ddf9e1864e0cbca8ab18d140c3d0719e
minhasjatinder <91667539+minhasjatinder@users.noreply.github.com>
2022-07-24 10:38:57 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100 bg-light text-center">
+        <p>made with love</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
